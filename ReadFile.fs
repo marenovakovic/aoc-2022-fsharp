c753b1fca8f468c8c334fb8045a5b140220589e8
markonovakovic <graornovakovic@gmail.com>
2022-12-06 19:35:52 +0100

    using __SOURCE_DIRECTORY__ instead of literal path

--- ReadFile.fs
+++ ReadFile.fs
@@ -3,12 +3,9 @@ module aoc_2022_fsharp.ReadFile
 open System.IO
 
 let readFile fileName =
-    let basePath =
-        "/Users/markonovakovic/Developer/RiderProjects/aoc-2022-fsharp/"
-
     seq {
         use reader =
-            new StreamReader(File.OpenRead($"{basePath}/{fileName}"))
+            new StreamReader(File.OpenRead($"{__SOURCE_DIRECTORY__}/{fileName}"))
 
         while not reader.EndOfStream do
             yield reader.ReadLine()
