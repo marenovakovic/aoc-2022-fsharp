50b2e5aadeaa3658e0fce13ee99c836f99eac94b
markonovakovic <graornovakovic@gmail.com>
2022-12-07 13:43:41 +0100

    day one part one

--- day1/Day1.fs
+++ day1/Day1.fs
@@ -1,3 +1,16 @@
 module aoc_2022_fsharp.day1.partOne
 
-let partOne input = 1000
+let private splitCaloriesList (calories: seq<string>) =
+    calories |> Seq.map (fun s -> s.Split "\n")
+
+let private sumCaloriesPerElf caloriesList =
+    caloriesList
+    |> Seq.map (fun calories -> calories |> Array.map int)
+    |> Seq.map Array.sum
+
+let partOne (input: string) =
+    input.Split "\n\n"
+    |> Seq.ofArray
+    |> splitCaloriesList
+    |> sumCaloriesPerElf
+    |> Seq.max
--- day1/Day1Test.fs
+++ day1/Day1Test.fs
@@ -3,7 +3,14 @@ module aoc_2022_fsharp.day1.Day1Test
 open NUnit.Framework
 open FsUnit
 open aoc_2022_fsharp.day1.partOne
+open aoc_2022_fsharp.ReadFile
 
 [<Test>]
-let rec ``part one`` () =
+let ``part one`` () =
     "1000" |> partOne |> should equal 1000
+    "2000" |> partOne |> should equal 2000
+    "2000\n\n1000" |> partOne |> should equal 2000
+    "1000\n\n2000" |> partOne |> should equal 2000
+    "1000\n2000\n\n2000" |> partOne |> should equal 3000
+    "1000\n2000\n\n4000" |> partOne |> should equal 4000
+    "day1/test_input.txt" |> readFile |> partOne |> should equal 24000
