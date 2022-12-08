0edd8faa1b46c76977f0ec7f098ed24f874e5baa
markonovakovic <graornovakovic@gmail.com>
2022-12-08 18:03:22 +0100

    part one

--- day2/Day2.fs
+++ day2/Day2.fs
@@ -1,5 +1,6 @@
 module aoc_2022_fsharp.day2.Day2
 
+open System
 open aoc_2022_fsharp.ReadFile
 open aoc_2022_fsharp.Split
 
@@ -13,10 +14,14 @@ type Play =
 let private translation =
     Map [ "A", Rock; "B", Paper; "C", Scissors; "X", Rock; "Y", Paper; "Z", Scissors ]
 
+let private wins = [ (Rock, Paper); (Paper, Scissors); (Scissors, Rock) ]
+
+let private loses = [ (Paper, Rock); (Scissors, Paper); (Rock, Scissors) ]
+
+let private score = Map [ Rock, 1; Paper, 2; Scissors, 3 ]
+
 let private parseTurns strategy =
     strategy |> split "\n" |> List.map (split " ")
-
-let readStrategy fileName = fileName |> readFile
 
 let private mapTurns turns =
     turns
@@ -26,3 +31,18 @@ let private mapTurns turns =
         | _ -> raise IllegalState)
 
 let parseStrategy strategy = strategy |> parseTurns |> mapTurns
+
+let private scoreTurn turn =
+    let _, player = turn
+
+    if wins |> List.contains turn then 6 + score[player]
+    elif loses |> List.contains turn then score[player]
+    else 3 + score[player]
+
+let scoreStrategy (strategy: (Play * Play) list) =
+    let rec loop score turns =
+        match turns with
+        | turn :: rest -> loop (score + scoreTurn turn) rest
+        | _ -> score
+
+    loop 0 strategy
--- day2/Day2Test.fs
+++ day2/Day2Test.fs
@@ -3,10 +3,47 @@ module aoc_2022_fsharp.day2.Day2Test
 open NUnit.Framework
 open FsUnit
 open aoc_2022_fsharp.day2.Day2
+open aoc_2022_fsharp.ReadFile
 
 [<Test>]
 let ``should parses input`` () =
     "day2/test_input.txt"
-    |> readStrategy
+    |> readFile
     |> parseStrategy
     |> should equal [ (Rock, Paper); (Paper, Rock); (Scissors, Scissors) ]
+
+[<Test>]
+let ``should score rock paper`` () =
+    [ (Rock, Paper) ] |> scoreStrategy |> should equal 8
+
+[<Test>]
+let ``should score rock rock`` () =
+    [ (Rock, Rock) ] |> scoreStrategy |> should equal 4
+
+[<Test>]
+let ``should score rock scissors`` () =
+    [ (Rock, Scissors) ] |> scoreStrategy |> should equal 3
+
+[<Test>]
+let ``should score paper scissors`` () =
+    [ (Paper, Scissors) ] |> scoreStrategy |> should equal 9
+
+[<Test>]
+let ``should score two turns`` () =
+    [ (Rock, Paper); (Paper, Scissors) ] |> scoreStrategy |> should equal 17
+
+[<Test>]
+let ``should score test game`` () =
+    "day2/test_input.txt"
+    |> readFile
+    |> parseStrategy
+    |> scoreStrategy
+    |> should equal 15
+
+[<Test>]
+let ``should score actual game (part one)`` () =
+    "day2/input.txt"
+    |> readFile
+    |> parseStrategy
+    |> scoreStrategy
+    |> should equal 12458
