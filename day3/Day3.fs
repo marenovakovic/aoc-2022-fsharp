4a1c7ee52604070fd7067feebf47587bc5fa12c7
markonovakovic <graornovakovic@gmail.com>
2022-12-15 14:57:49 +0100

    parse two chars

--- day3/Day3.fs
+++ day3/Day3.fs
@@ -1,2 +1,3 @@
-module aoc_2022_fsharp.day3.Day2
+module aoc_2022_fsharp.day3.Day3
 
+let parseRucksack (items: string) = [ string items[0]; string items[1] ]
--- day3/Day3Test.fs
+++ day3/Day3Test.fs
@@ -1,2 +1,9 @@
 module aoc_2022_fsharp.day3.Day3Test
 
+open NUnit.Framework
+open FsUnit
+open Day3
+
+[<Test>]
+let ``can parse two chars`` () =
+    "ab" |> parseRucksack |> should equal [ "a"; "b" ]
