03ad02087f44c21ada5097ff837fef5d9fd25eae
markonovakovic <graornovakovic@gmail.com>
2022-12-16 12:48:26 +0100

    type can be infered

--- day3/Day3.fs
+++ day3/Day3.fs
@@ -9,7 +9,7 @@ let parseRucksack (items: string) =
     let half = length / 2
     items[0 .. half - 1], items[half..length]
 
-let findCommonItem (rucksack: string * string) =
+let findCommonItem rucksack =
     let firstHalf = fst rucksack |> Seq.toList
     let secondHalf = snd rucksack |> Seq.toList
     let intersection = Set.intersect (Set.ofList firstHalf) (Set.ofList secondHalf)
