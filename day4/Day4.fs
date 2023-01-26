6a159584e0797589e4c78805c999035b6d0fb1a1
markonovakovic <graornovakovic@gmail.com>
2023-01-26 18:54:04 +0100

    range to section

--- day4/Day4.fs
+++ day4/Day4.fs
@@ -1,3 +1,8 @@
 module aoc_2022_fsharp.day4.Day4
 
+open aoc_2022_fsharp.Split
 
+let rangeToSections range =
+    let split = range |> split "-" |> List.map string |> List.map int
+
+    [ split[0] .. split[1] ]
--- day4/Day4Test.fs
+++ day4/Day4Test.fs
@@ -1,3 +1,9 @@
 module aoc_2022_fsharp.day4.Day4Test
 
+open NUnit.Framework
+open FsUnit
+open Day4
 
+[<Test>]
+let ``creates section set from range`` () =
+    "2-4" |> rangeToSections |> should equal [ 2; 3; 4 ]
