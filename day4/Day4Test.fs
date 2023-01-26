9c6b582b899d54d648da1a7d3a60a917199fd270
markonovakovic <graornovakovic@gmail.com>
2023-01-26 20:23:01 +0100

    refactoring

--- day4/Day4Test.fs
+++ day4/Day4Test.fs
@@ -10,8 +10,10 @@ let ``creates section set from range`` () =
 
 [<Test>]
 let ``creates pair assignment from line`` () =
-    "2-4,4-6" |> lineToAssignment |> should equal [[2; 3; 4]; [4; 5; 6]]
+    "2-4,4-6" |> lineToAssignment |> should equal [ [ 2; 3; 4 ]; [ 4; 5; 6 ] ]
 
 [<Test>]
 let ``creates list of pair assignments`` () =
-    "2-4,4-6\n3-5,6-8" |> pairAssignments |> should equal [[[2; 3; 4]; [4; 5; 6]]; [[3; 4; 5]; [6; 7; 8]]]
+    "2-4,4-6\n3-5,6-8"
+    |> pairAssignments
+    |> should equal [ [ [ 2; 3; 4 ]; [ 4; 5; 6 ] ]; [ [ 3; 4; 5 ]; [ 6; 7; 8 ] ] ]
