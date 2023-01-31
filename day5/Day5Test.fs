ff875212560868a22e79b678e60f9cafbeb0b0f7
markonovakovic <graornovakovic@gmail.com>
2023-01-31 13:17:07 +0100

    refactoring tests -> making string literals more readable. idk is this good idea but future tests will show

--- day5/Day5Test.fs
+++ day5/Day5Test.fs
@@ -3,23 +3,27 @@ module aoc_2022_fsharp.day5.Day5Test
 open NUnit.Framework
 open FsUnit
 open Day5
+open FSharpPlus
 
 [<Test>]
 let ``splits stack diagram and moving instructions`` () =
-    """[A] [B]
+    """
+[A] [B]
  1   2
 
 move 1 from 2 to 1"""
     |> separateStacksAndMoves
     |> should
         equal
-        [ """[A] [B]
+        [ """
+[A] [B]
  1   2"""
           """move 1 from 2 to 1""" ]
 
 [<Test>]
 let ``get number of stacks from last line of the diagram`` () =
-    """[A] [B] [C]
+    """
+[A] [B] [C]
  1   2   3
 
 move 1 from 2 to 1"""
@@ -32,7 +36,10 @@ let ``parse move`` () =
 
 [<Test>]
 let ``parse moves`` () =
-    """move 1 from 2 to 3
-move 3 from 2 to 1"""
+    """
+    move 1 from 2 to 3
+    move 3 from 2 to 1
+    """
+    |> String.trimWhiteSpaces
     |> parseMoves
     |> should equal [ [ 1; 2; 3 ]; [ 3; 2; 1 ] ]
