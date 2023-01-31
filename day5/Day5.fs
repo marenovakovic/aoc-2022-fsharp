07cdc20c377309dbb6904db7995e4419e4d8ece4
markonovakovic <graornovakovic@gmail.com>
2023-01-31 12:38:09 +0100

    split stack and move instructions

--- day5/Day5.fs
+++ day5/Day5.fs
@@ -1,2 +1,5 @@
 module aoc_2022_fsharp.day5.Day5
 
+open aoc_2022_fsharp.Split
+
+let separateStacksAndMoves (input: string) = input |> split "\n\n"
--- day5/Day5Test.fs
+++ day5/Day5Test.fs
@@ -1,2 +1,18 @@
 module aoc_2022_fsharp.day5.Day5Test
 
+open NUnit.Framework
+open FsUnit
+open Day5
+
+[<Test>]
+let ``splits stack diagram and moving instructions`` () =
+    """[A] [B]
+ 1   2
+
+move 1 from 2 to 1"""
+    |> separateStacksAndMoves
+    |> should
+        equal
+        [ """[A] [B]
+ 1   2"""
+          """move 1 from 2 to 1""" ]
