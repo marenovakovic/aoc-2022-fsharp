820b1215a42d7ae12c54a5382335adf6c746fe5f
markonovakovic <graornovakovic@gmail.com>
2023-10-13 19:41:17 +0200

    renaming. part two introduces new marker

--- day6/Day6.fs
+++ day6/Day6.fs
@@ -2,13 +2,15 @@ module aoc_2022_fsharp.day6.Day6
 
 open FSharpPlus
 
-let private windowSize = 4
+let private packetMarkerSize = 4
 
 let private findMarkerPosition' parts =
     let indexOfWindowWithDistinctCharacters =
-        parts |> Seq.findIndex (fun x -> Array.length x = windowSize)
+        parts |> Seq.findIndex (fun x -> Array.length x = packetMarkerSize)
 
-    indexOfWindowWithDistinctCharacters + windowSize
+    indexOfWindowWithDistinctCharacters + packetMarkerSize
 
 let findMarkerPosition s =
-    Seq.windowed windowSize s |> Seq.map Array.distinct |> findMarkerPosition'
+    Seq.windowed packetMarkerSize s
+    |> Seq.map Array.distinct
+    |> findMarkerPosition'
