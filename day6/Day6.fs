843642940fbe530356eb9031e8e2b6682847b624
markonovakovic <graornovakovic@gmail.com>
2023-10-13 19:39:47 +0200

    part one, solved

--- day6/Day6.fs
+++ day6/Day6.fs
@@ -1,1 +1,14 @@
 module aoc_2022_fsharp.day6.Day6
+
+open FSharpPlus
+
+let private windowSize = 4
+
+let private findMarkerPosition' parts =
+    let indexOfWindowWithDistinctCharacters =
+        parts |> Seq.findIndex (fun x -> Array.length x = windowSize)
+
+    indexOfWindowWithDistinctCharacters + windowSize
+
+let findMarkerPosition s =
+    Seq.windowed windowSize s |> Seq.map Array.distinct |> findMarkerPosition'
--- day6/Day6Test.fs
+++ day6/Day6Test.fs
@@ -1,1 +1,35 @@
 module aoc_2022_fsharp.day6.Day6Test
+
+open aoc_2022_fsharp.ReadFile
+open Day6
+open NUnit.Framework
+open FsUnit
+open aoc_2022_fsharp
+
+[<Test>]
+let ``find marker position for four different characters`` () =
+    "abcd" |> findMarkerPosition |> should equal 4
+
+[<Test>]
+let ``find marker position for five characters first two of which are the same`` () =
+    "aabcd" |> findMarkerPosition |> should equal 5
+
+[<Test>]
+let ``find marker position for six characters first two of which are the same`` () =
+    "aabcde" |> findMarkerPosition |> should equal 5
+
+[<Test>]
+let ``find marker position for seven characters first two of which are the same`` () =
+    "aabcdef" |> findMarkerPosition |> should equal 5
+
+[<Test>]
+let ``find marker position for eight characters first four of which are the same`` () =
+    "aaaabcde" |> findMarkerPosition |> should equal 7
+
+[<Test>]
+let ``part one test input`` () =
+    "day6/test_input.txt" |> readFile |> findMarkerPosition |> should equal 7
+
+[<Test>]
+let ``part one real input`` () =
+    "day6/input.txt" |> readFile |> findMarkerPosition |> should equal 1598
