79556713d087a3886d34be99a9e80c0520a80615
markonovakovic <graornovakovic@gmail.com>
2023-10-13 18:55:12 +0200

    saving progress

--- day5/Day5.fs
+++ day5/Day5.fs
@@ -1,38 +1,14 @@
 module aoc_2022_fsharp.day5.Day5
 
 open System
-open FSharpPlus
 open aoc_2022_fsharp.Split
 
-let separateStacksAndMoves input = input |> split "\n\n"
+let separateStacksAndMoves s = split "\n\n" s
 
-let determineNumberOfStacks input =
-    input
-    |> separateStacksAndMoves
-    |> List.head
-    |> split "\n"
-    |> List.last
-    |> String.filter Char.IsDigit
-    |> String.length
+let determineNumberOfStacks stacks =
+    stacks |> split "\n" |> List.last |> String.filter Char.IsDigit |> String.length
 
-let parseMove move =
-    move
-    |> String.filter Char.IsDigit
-    |> String.toList
-    |> List.map string
-    |> List.map int
+let private removeLast list =
+    list |> List.rev |> List.skip 1 |> List.rev
 
-let parseMoves moves =
-    moves |> split "\n" |> List.map parseMove
-
-let private notEmpty s = s <> ""
-
-let private dropLast (list: string list) = (list.Length - 1, list) ||> List.take
-
-let extractStackNames input =
-    input
-    |> split "\n"
-    |> dropLast
-    |> List.map (split " ")
-    |> List.collect id
-    |> List.map (fun x -> string x[1])
+let extractStackNames s = s |> split "\n" |> removeLast
--- day5/Day5Test.fs
+++ day5/Day5Test.fs
@@ -3,65 +3,31 @@ module aoc_2022_fsharp.day5.Day5Test
 open NUnit.Framework
 open FsUnit
 open Day5
-open FSharpPlus
 
 [<Test>]
-let ``splits stack diagram and moving instructions`` () =
-    """
-[A] [B]
+let ``split diagram and moves`` () =
+    """[A] [B]
  1   2
 
 move 1 from 2 to 1"""
     |> separateStacksAndMoves
     |> should
         equal
-        [ """
-[A] [B]
+        [ """[A] [B]
  1   2"""
           """move 1 from 2 to 1""" ]
 
 [<Test>]
-let ``get number of stacks from last line of the diagram`` () =
-    """
-[A] [B] [C]
- 1   2   3
-
-move 1 from 2 to 1"""
-    |> determineNumberOfStacks
+let ``determines number of stack by counting last line`` () =
+    determineNumberOfStacks
+        """[A] [B] [C]
+ 1   2   3"""
     |> should equal 3
 
 [<Test>]
-let ``parse move`` () =
-    "move 1 from 2 to 1" |> parseMove |> should equal [ 1; 2; 1 ]
-
-[<Test>]
-let ``parse moves`` () =
-    """
-    move 1 from 2 to 3
-    move 3 from 2 to 1
-    """
-    |> String.trimWhiteSpaces
-    |> parseMoves
-    |> should equal [ [ 1; 2; 3 ]; [ 3; 2; 1 ] ]
-
-[<Test>]
-let ``extract name for single stack`` () =
-    """[A]
- 1"""
-    |> extractStackNames
-    |> should equal [ "A" ]
-
-[<Test>]
-let ``extract name for two stacks`` () =
-    """[A] [B]
- 1   1"""
-    |> extractStackNames
-    |> should equal [ "A"; "B" ]
-
-[<Test>]
-let ``extract name for three stacks`` () =
-    """    [A]    
-[A] [B] [C]
- 1   2   1"""
-    |> extractStackNames
-    |> should equal [ [ " "; "A"; " " ]; [ "A"; "B"; "C" ] ]
+let ``extracts stack names`` () =
+    extractStackNames
+        """    [A]      
+[B] [C] [D]
+ 1   2   3"""
+    |> should equal [ [ " "; "A"; " " ]; [ "B"; "C"; "D" ] ]
