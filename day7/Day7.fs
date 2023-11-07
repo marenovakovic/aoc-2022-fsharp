ececcf9a8de21fe5555495296ca5b4c95371f8dc
markonovakovic <graornovakovic@gmail.com>
2023-11-07 13:30:08 +0100

    day 7, parsing lines

--- day7/Day7.fs
+++ day7/Day7.fs
@@ -1,1 +1,19 @@
 module aoc_2022_fsharp.day7.Day7
+
+open FSharpPlus
+open System
+open aoc_2022_fsharp.Split
+
+type TerminalLine =
+    | Command of command: string
+    | Directory of name: string
+    | File of size: int * name: string
+
+let private determineLine =
+    function
+    | "$" :: rest -> Command(rest |> List.head)
+    | "dir" :: [ name ] -> Directory name
+    | size :: [ name ] -> File(int size, name)
+    | _ -> raise (Exception())
+
+let parseLine = split " " >> determineLine
--- day7/Day7Test.fs
+++ day7/Day7Test.fs
@@ -1,1 +1,12 @@
 module aoc_2022_fsharp.day7.Day7Test
+
+open NUnit.Framework
+open FsUnit
+open Day7
+
+[<Test>]
+let ``parse line`` () =
+    "$ cd /" |> parseLine |> should equal (TerminalLine.Command "cd")
+    "$ ls" |> parseLine |> should equal (TerminalLine.Command "ls")
+    "dir a" |> parseLine |> should equal (TerminalLine.Directory "a")
+    "123 a" |> parseLine |> should equal (TerminalLine.File(123, "a"))
