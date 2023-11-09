ed1c2979193e78fe26457851aaf3720a11bab902
markonovakovic <graornovakovic@gmail.com>
2023-11-09 23:12:53 +0100

    deleting pi

--- day7/Day7Test.fs
+++ day7/Day7Test.fs
@@ -6,7 +6,7 @@ open Day7
 
 [<Test>]
 let ``parse line`` () =
-    "$ cd /" |> parseLine |> should equal (πEnterDirectory "/")
+    "$ cd /" |> parseLine |> should equal (EnterDirectory "/")
     "$ ls" |> parseLine |> should equal ListFiles
     "dir a" |> parseLine |> should equal (Directory "a")
     "123 a" |> parseLine |> should equal (File(123, "a"))
